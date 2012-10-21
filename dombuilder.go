64fc0e1acce870d31e763bc1b1922a1860cf980f
Sebastian Mitterle <sebastianmitterle@mathume.com>
2012-10-21 13:04:58 +0200

    fixed missing errors in dombuilder.Build() and added tests

--- dombuilder.go
+++ dombuilder.go
@@ -3,81 +3,76 @@ package dom
 import (
 	"encoding/xml"
 	"io"
-	"errors"
-	"fmt"
 )
 
-type dombuilder struct{
-	d DOM
+type dombuilder struct {
+	d      DOM
 	reader io.Reader
-	store DOMStore
+	store  DOMStore
 }
 
 // creates and initializes IDOMBuilder structure
 // only implements building dom from xml file
-func NewDOMBuilder(reader io.Reader, store DOMStore) DOMBuilder{
+func NewDOMBuilder(reader io.Reader, store DOMStore) DOMBuilder {
 	db := new(dombuilder)
 	db.reader = reader
 	db.store = store
 	return db
 }
 
-func (db *dombuilder)Reader()(reader io.Reader){
+func (db *dombuilder) Reader() (reader io.Reader) {
 	return db.reader
 }
 
-func (db *dombuilder)Build() (dom DOM, err error){
-	defer func() {
-		if r:=recover(); r!=nil {
-			err = errors.New(fmt.Sprint(r))
-			return
-		}
-	}()
+func (db *dombuilder) Build() (dom DOM, err error) {
 	
-	dom = db.convertDecoderToDOM()
+	dom,err = db.convertDecoderToDOM()
 	return
 }
 
-func (db *dombuilder)convertDecoderToDOM() DOM{
+func (db *dombuilder) convertDecoderToDOM()(dom DOM, err error) {
 
 	dc := xml.NewDecoder(db.reader)
-	dom := db.store.NewDOM()
-	build_subtree(dc, dom)
-	return dom
+	dom = db.store.NewDOM()
+	err = build_subtree(dc, dom)
+	return
 }
 
-func build_subtree(dc *xml.Decoder, n Node){
-	tok,_ := dc.Token()
-	switch typ:=tok.(type){
+func build_subtree(dc *xml.Decoder, n Node) (err error) {
+	tok, err := dc.Token()
+	if err != nil {
+		return
+	}
+	switch typ := tok.(type) {
 	case xml.StartElement:
-		el := n.Store().CreateElement(typ.Name.Space, typ.Name.Local, convert_Attr(typ.Attr,n.Store()))
+		el := n.Store().CreateElement(typ.Name.Space, typ.Name.Local, convert_Attr(typ.Attr, n.Store()))
 		el.SetParent(n)
 		n.AppendChildNode(el)
-		build_subtree(dc, el)
+		err = build_subtree(dc, el)
 	case xml.CharData:
 		txt := n.Store().CreateText(string(typ))
 		txt.SetParent(n)
 		n.AppendChildNode(txt)
-		build_subtree(dc, n)
+		err = build_subtree(dc, n)
 	case xml.Comment:
 		a := n.Store().CreateComment(string(typ))
 		a.SetParent(n)
 		n.AppendChildNode(a)
-		build_subtree(dc,n)
+		err = build_subtree(dc, n)
 	case xml.Directive:
 		d := n.Store().CreateDirective(string(typ))
 		d.SetParent(n)
 		n.AppendChildNode(d)
-		build_subtree(dc,n)
+		err = build_subtree(dc, n)
 	case xml.ProcInst:
 		pi := n.Store().CreateProcInst(typ.Target, string(typ.Inst))
 		pi.SetParent(n)
 		n.AppendChildNode(pi)
-		build_subtree(dc,n)
+		err = build_subtree(dc, n)
 	case xml.EndElement:
-		switch pTyp := n.Parent().(type){
+		switch pTyp := n.Parent().(type) {
 		case Element:
-			build_subtree(dc, pTyp)
+			err = build_subtree(dc, pTyp)
 		default:
 			return
 		}
@@ -85,9 +80,9 @@ func build_subtree(dc *xml.Decoder, n Node){
 	return
 }
 
-func convert_Attr(a []xml.Attr,store DOMStore) []Attribute{
-	as := make([]Attribute,len(a))
-	for i:= 0; i<len(a); i++ {
+func convert_Attr(a []xml.Attr, store DOMStore) []Attribute {
+	as := make([]Attribute, len(a))
+	for i := 0; i < len(a); i++ {
 		as[i] = store.CreateAttribute(a[i].Name.Space, a[i].Name.Local, a[i].Value)
 	}
 	return as
--- dombuilder_test.go
+++ dombuilder_test.go
@@ -1,48 +1,72 @@
 package dom
 
 import (
-	"testing"
 	"encoding/xml"
 	"strings"
+	"testing"
 )
 
-func TestConvertDecoderToDOM_ReadsDeclaration(t *testing.T){
-	text,expected_declaration := declaration_and_empty_root()
-	db := NewDOMBuilder(strings.NewReader(text),NewDOMStore())
-	dom,_ := db.Build()
+func TestConvertDecoderToDOM_ReadsDeclaration(t *testing.T) {
+	text, expected_declaration := declaration_and_empty_root()
+	db := NewDOMBuilder(strings.NewReader(text), NewDOMStore())
+	dom, _ := db.Build()
 	d := dom.Declaration()
-	
-	if(d.String() != expected_declaration){
+
+	if d.String() != expected_declaration {
 		t.Fail()
 	}
 }
 
-func TestConvertDecoderToDOM_ReadsEmptyRoot(t *testing.T){
-	db := NewDOMBuilder(strings.NewReader(empty_root()),NewDOMStore())
+func TestConvertDecoderToDOM_ReadsEmptyRoot(t *testing.T) {
+	db := NewDOMBuilder(strings.NewReader(empty_root()), NewDOMStore())
 	expected := split_empty_root()
-	dom,_ := db.Build()
+	dom, _ := db.Build()
 	r := dom.Root()
-	t.Log(r)
-	t.Log(expected)
-	if(r.String() != expected){
+
+	if r.String() != expected {
 		t.Fail()
 	}
 }
 
-func TestConvertDecoderToDOM_Build_RaisesError_WithNonXml(t *testing.T){
-	t.Fatal("Test not implemented yet.")
+func TestConvertDecoderToDOM_Build_ReturnsError_WithNonXml(t *testing.T) {
+	nonXml := "abc"
+	db := NewDOMBuilder(strings.NewReader(nonXml), NewDOMStore())
+	_, err := db.Build()
+	if err == nil {
+		t.Fail()
+	}
 }
 
-func split_empty_root()(root string){
+func TestConvertDecoderToDOM_MissingEndTag_ReturnsError(t *testing.T) {
+	xmlMissingEndTag := "<root>"
+	db := NewDOMBuilder(strings.NewReader(xmlMissingEndTag), NewDOMStore())
+	_, err := db.Build()
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestDecoderAtMissingEndTag(t *testing.T) {
+	reader := strings.NewReader("<rootwithoutendtag>")
+	dec := xml.NewDecoder(reader)
+	t.Log(dec.Strict)
+	_, err := dec.Token()
+	_, err = dec.Token()
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func split_empty_root() (root string) {
 	return "<root></root>"
 }
 
-func declaration_and_empty_root() (text, expected_declaration string){
+func declaration_and_empty_root() (text, expected_declaration string) {
 	text = xml.Header + empty_root()
-	expected_declaration = strings.Split(xml.Header,"\n")[0]
+	expected_declaration = strings.Split(xml.Header, "\n")[0]
 	return
 }
 
-func empty_root() string{
+func empty_root() string {
 	return "<root/>"
 }
